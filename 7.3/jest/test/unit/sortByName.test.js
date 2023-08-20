c565c6a2ee97158b9fd45b0d059bc9a0c0aef22e
anna.yurasova <anna.yurasova@ssi-schafer.ru>
2023-08-20 13:00:14 +0400

    add test, coverage code

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,31 @@
 const sorting = require("../../app");
 
 describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+  test("Books names should be sorted in ascending order", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const result = sorting.sortByName(input);
+
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+
+    expect(result).toEqual(expected);
+  });
+
+  test("Books names should not be sorted", () => {
+    const input = ["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"];
+
+    const result = sorting.sortByName(input);
+
+    const expected = ["Гарри Поттер", "Гарри Поттер", "Гарри Поттер"];
+
+    expect(result).toEqual(expected);
   });
 });
